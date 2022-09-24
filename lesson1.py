7e176c4bb36a92ff12912579223b0054641908d5
LionelMaessi <114161715+LionelMaessi@users.noreply.github.com>
2022-09-24 12:35:51 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+try:
+    pass
+except Exception as e:
+    raise
+    # REVIEW: make cake
+    def foo():
+        doc = "The  property."
+        def fget(self):
+            return self._
+        def fset(self, value):
+            self._ = value
+        def fdel(self):
+            del self._
+        return locals()
+     = property(**()) 
